ff9140bdc6c24da7cbb218da4381ceb21e17d22d
Jaspal Singh <sjaspal.0027@gmail.com>
2024-02-28 23:03:20 +0800

    added slice

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,6 +4,7 @@ import { PORT, mongoDB } from "./config.js";
 import JobOrderRouter from "./routes/jobOrder.js";
 import mongoose from "mongoose";
 import router from "./routes/user.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
 const app = express();
@@ -26,6 +27,7 @@ mongoose
 // MIDDLEWARE
 app.use("/user", router);
 app.use("/joborder", JobOrderRouter);
+app.use(cookieParser());
 
 // ROUTING
 app.post("/crt", async (req, res) => {
--- FrontEnd/src/state/store.ts
+++ FrontEnd/src/state/store.ts
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobOrderListReducer from "./joborderlist/jobOrderListSlice";
 import jobOrderReducer from "./joborder/jobOrderSlice";
-
+import authReducer from "../slices/authSlice";
+import { apiSlice } from "../slices/apiSlice";
+import { usersApiSlice } from "../slices/userApiSlice";
 export const store = configureStore({
   reducer: {
     joborderlist: jobOrderListReducer,
     joborder: jobOrderReducer,
+    auth: authReducer,
+    [usersApiSlice.reducerPath]: usersApiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersApiSlice.middleware),
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
