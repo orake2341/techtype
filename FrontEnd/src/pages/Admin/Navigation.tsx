9b0610deb2f96eecccda106eeb60ad01127a250e
Nathan <shii.max19@gmail.com>
2024-03-09 13:28:39 +0800

    commit

--- FrontEnd/src/pages/Admin/Navigation.tsx
+++ FrontEnd/src/pages/Admin/Navigation.tsx
@@ -6,15 +6,6 @@ const Navigation = () => {
     <>
       <nav className="flex flex-col w-1/6 h-screen bg-gradient-to-b from-white to-CCCCCCC items-center gap-y-4">
         <figure className="flex flex-col h-1/4 w-full items-center justify-center">
-          <img
-            src={img}
-            width="150"
-            alt="a profile"
-            className="rounded-full "
-          />
-          <figcaption>
-            <h3>Mark Angelo P. Baes</h3>
-          </figcaption>
         </figure>
 
         <div className="flex flex-col h-3/4 w-full items-center gap-y-96">
--- FrontEnd/src/pages/CustomerPortal/Navigation.tsx
+++ FrontEnd/src/pages/CustomerPortal/Navigation.tsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-import img from "../../assets/imgs/member4.jpg";
 import { logout } from "../../slices/authSlice";
 import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../state/store";
 import { useNavigate } from "react-router-dom";
+
 type NavigationProps = {
   visible: boolean;
 };
@@ -11,60 +10,48 @@ type NavigationProps = {
 const Navigation = ({ visible }: NavigationProps) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  
   const logoutAction = () => {
     dispatch(logout(""));
     navigate("/login");
   };
+
   return (
     <>
       <nav
         className={
           !visible
             ? ""
-            : "flex flex-col w-1/6 h-screen bg-gradient-to-b from-white to-CCCCCCC items-center gap-y-4"
+            : "flex flex-col w-1/6 h-screen bg-gradient-to-b from-white to-gray-300 items-center justify-between"
         }
       >
-        <figure className="flex flex-col h-1/4 w-full items-center justify-center">
-          <img
-            src={img}
-            width="150"
-            alt="a profile"
-            className="rounded-full "
-          />
-          <figcaption>
-            <h3>Mark Angelo P. Baes</h3>
-          </figcaption>
-        </figure>
-
-        <div className="flex flex-col h-3/4 w-full items-center gap-y-96">
-          <div className="flex flex-col items-center gap-y-4">
-            <Link
-              to="joborder"
-              className="bg-white hover:bg-CACACAC text-black font-bold py-2 px-4 rounded-full"
-            >
-              Job Order
-            </Link>
-            <Link
-              to="paymenthistory"
-              className="bg-white hover:bg-CACACAC text-black font-bold py-2 px-4 rounded-full"
-            >
-              Payment History
-            </Link>
-            <Link
-              to="settings"
-              className="bg-white hover:bg-CACACAC text-black font-bold py-2 px-4 rounded-full"
-            >
-              Settings
-            </Link>
-          </div>
-          <div>
-            <button
-              className="bg-white hover:bg-CACACAC text-black font-bold py-2 px-4 rounded-full"
-              onClick={logoutAction}
-            >
-              Logout
-            </button>
-          </div>
+        <div className="flex flex-col items-center gap-y-4 pt-8">
+          <Link
+            to="joborder"
+            className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full"
+          >
+            Job Order
+          </Link>
+          <Link
+            to="paymenthistory"
+            className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full"
+          >
+            Payment History
+          </Link>
+          <Link
+            to="settings"
+            className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full"
+          >
+            Settings
+          </Link>
+        </div>
+        <div className="pb-8">
+          <button
+            className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full"
+            onClick={logoutAction}
+          >
+            Logout
+          </button>
         </div>
       </nav>
     </>
